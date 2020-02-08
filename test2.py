c4e3378346395ad5dcc3f51a2109d1caf7f4f79c
madmax3000 <johnjose223@gmail.com>
2020-02-08 21:22:46 +0530

    1.the solution is being printed now seperately after optimization 2.better visual method to analyse solutions

--- test2.py
+++ test2.py
@@ -51,6 +51,6 @@ def input(abbas):
     if (abbas == "Enter the max and min limits of the output in the following format \n ( max,min)"):
         return '6,5.9'
     if (abbas == "Enter the no: of iterations"):
-        return 10
+        return 100
     if (abbas == "do you want to keep same initialisation file for all run?\ny/n\n"):
         return 'y'
