094b5bc30de5bf4470be0e949098f33854e474cb
madmax3000 <johnjose223@gmail.com>
2020-03-17 20:56:47 +0530

    made the software free from unnecessaryfiles

--- globalfile.py
+++ globalfile.py
@@ -1,5 +1,5 @@
-diagramarray=['Inverter_ckt.csv','single_phase_xmer.csv','rectifier_ckt.csv'] #csv file
-paramsarray=['Inverter_ckt.csv_params.csv','single_phase_xmer_params.csv','rectifier_ckt_params.csv']  #params file is read here
+diagramarray=['ckt1.csv','ckt2.csv','ckt3.csv'] #csv file
+paramsarray=['ckt1_params.csv','ckt2_params.csv','ckt3_params.csv']  #params file is read here
 outputarray=['ckt_output1.dat','ckt_output2.dat'] # output file details are sent here
 # all csv files should be in the same order params as well as diagram all files must be mentioned in same order
-controlvariable=[0.001061465077164266]
+controlvariable=[] #can be added as list
